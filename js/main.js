7e181d577dd4c26ad37112c15f5f12247f8a48b7
andretassis <tassisandre@gmail.com>
2023-10-24 11:56:42 -0300

    acerto de código

--- js/main.js
+++ js/main.js
@@ -1,42 +1,42 @@
 function cardHighlight(selector) {
-    let element = document.querySelector(selector)
+    let element = document.querySelector(selector);
 
-    element.classList.toggle('card_highlight')
+    element.classList.toggle('card_highlight');
 }
 
 function keyboardCardHighlight() {
 
-    let ingresso1 = document.getElementById("quinta")
-    let ingresso2 = document.getElementById("sexta")
-    let ingresso3 = document.getElementById("sabado")
-    let ingresso4 = document.getElementById("domingo")
+    let ingresso1 = document.getElementById("quinta");
+    let ingresso2 = document.getElementById("sexta");
+    let ingresso3 = document.getElementById("sabado");
+    let ingresso4 = document.getElementById("domingo");
     
     document.addEventListener("keydown", (event) => {
         if(event.code == "Digit1")
-            ingresso1.classList.add('card_highlight')
-            ingresso2.classList.remove('card_highlight')
-            ingresso3.classList.remove('card_highlight')
-            ingresso4.classList.remove('card_highlight')
+            ingresso1.classList.add('card_highlight');
+            ingresso2.classList.remove('card_highlight');
+            ingresso3.classList.remove('card_highlight');
+            ingresso4.classList.remove('card_highlight');
 
         if(event.code == "Digit2") {
-            ingresso1.classList.remove('card_highlight')
-            ingresso2.classList.add('card_highlight')
-            ingresso3.classList.remove('card_highlight')
-            ingresso4.classList.remove('card_highlight')
+            ingresso1.classList.remove('card_highlight');
+            ingresso2.classList.add('card_highlight');
+            ingresso3.classList.remove('card_highlight');
+            ingresso4.classList.remove('card_highlight');
         }
 
         if(event.code == "Digit3") {
-            ingresso1.classList.remove('card_highlight')
-            ingresso2.classList.remove('card_highlight')
-            ingresso3.classList.add('card_highlight')
-            ingresso4.classList.remove('card_highlight')
+            ingresso1.classList.remove('card_highlight');
+            ingresso2.classList.remove('card_highlight');
+            ingresso3.classList.add('card_highlight');
+            ingresso4.classList.remove('card_highlight');
         }
 
         if(event.code == "Digit4") {
-            ingresso1.classList.remove('card_highlight')
-            ingresso2.classList.remove('card_highlight')
-            ingresso3.classList.remove('card_highlight')
-            ingresso4.classList.add('card_highlight')
+            ingresso1.classList.remove('card_highlight');
+            ingresso2.classList.remove('card_highlight');
+            ingresso3.classList.remove('card_highlight');
+            ingresso4.classList.add('card_highlight');
         }
     })
 }
